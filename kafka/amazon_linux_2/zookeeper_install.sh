2ba3ac320717b53a10041095bad834be2525b752
wisdomstar94 <wisdomstar94@gmail.com>
2022-03-27 14:49:12 +0900

    잘못된 명칭 수정

--- kafka/amazon_linux_2/zookeeper_install.sh
+++ kafka/amazon_linux_2/zookeeper_install.sh
@@ -14,9 +14,9 @@ main() {
   local install_path="/downloads"
   readonly install_path
 
-  # broker(kafka) 서버 정보들
-  local broker_servers=("broker1.host.com:2888:3888" "broker2.host.com:2888:3888" "broker3.host.com:2888:3888")
-  readonly broker_servers
+  # zookeeper 서버 정보들
+  local zookeeper_servers=("zookeeper1.host.com:2888:3888" "zookeeper2.host.com:2888:3888" "zookeeper3.host.com:2888:3888")
+  readonly zookeeper_servers
 
   # 다운로드 받을 대상에 대한 정보
   local target_name="apache-zookeeper-3.8.0-bin"
@@ -45,11 +45,11 @@ main() {
   # cp ./conf/zoo_sample.cfg ./conf/zoo.cfg # zoo_sample.cfg 파일을 zoo.cfg 파일로 복사
   echo -e "tickTime=2000\ndataDir=/var/lib/zookeeper\nclientPort=$port\nsyncLimit=5" > ./conf/zoo.cfg
   # sed -i'' -r -e "/dataDir=\/tmp\/zookeeper/c\dataDir=\/var\/lib\/zookeeper" ./conf/zoo.cfg # dataDir 경로를 /var/lib/zookeeper 로 교체
-  for ((i = 0; i < ${#broker_servers[@]}; i++)) ; do
+  for ((i = 0; i < ${#zookeeper_servers[@]}; i++)) ; do
     local server_id=`expr $i + 1`
-    echo -e "server.$server_id=${broker_servers[$i]}" >> ./conf/zoo.cfg
+    echo -e "server.$server_id=${zookeeper_servers[$i]}" >> ./conf/zoo.cfg
   done
-  # for i in "${broker_servers[@]}"
+  # for i in "${zookeeper_servers[@]}"
   # do
   #   echo "server.1=$i" >> ./conf/zoo.cfg
   # done
